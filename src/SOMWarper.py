f8f677a3d098bf57c2cf38f838e8cbca60892dff
Trajan32 <commode328@gmail.com>
2014-08-19 19:58:23 +0200

    Create SOMWarper.py

---
+++ src/SOMWarper.py
@@ -0,0 +1,18 @@
+class SOMWarper:
+	
+	def __init__(self):
+		
+		self.SOM_director
+		self.SOM_actors
+		self.SOM_writers
+		self.SOM_producers
+		
+		
+	def predict(self):
+		
+		# get the neuron firing for the provided person
+		
+		# return the distance between the 2D neuron position and the map's geometric center
+		
+			
+	def train(self):
